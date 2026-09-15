bdbbde3db218fef011b2ddd15ca5a8a6fdeda095
Pedro Henrique Molon Rodrigues <pedroh.molonr@gmail.com>
2026-09-15 10:21:33 -0300

    build: estrutura inicial do projeto com dependências

---
+++ src/main/java/com/pedromolon/eventticketsplatform/EventTicketsPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.pedromolon.eventticketsplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTicketsPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventTicketsPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pedromolon/eventticketsplatform/EventTicketsPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedromolon.eventticketsplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTicketsPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
