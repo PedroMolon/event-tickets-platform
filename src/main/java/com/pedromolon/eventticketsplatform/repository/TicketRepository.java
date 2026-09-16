47268be0d1cda2806a94b260c5a0652059aa33a1
Pedro Henrique Molon Rodrigues <pedroh.molonr@gmail.com>
2026-09-16 10:35:23 -0300

    fix: adjust name

--- src/main/java/com/pedromolon/eventticketsplatform/repository/TicketRepository.java
+++ src/main/java/com/pedromolon/eventticketsplatform/repository/TicketRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TickerRepository extends JpaRepository<Ticket, Long> {
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
     Page<Ticket> findByCustomerId(Long customerId, Pageable pageable);
 }
