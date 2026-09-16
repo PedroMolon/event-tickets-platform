da2e49df79ddc763840402f00d7e19f61e955c0d
Pedro Henrique Molon Rodrigues <pedroh.molonr@gmail.com>
2026-09-16 09:28:48 -0300

    fix: adjust flyway migration constraints and clean duplicate FKs Refs #2

--- src/main/resources/db/migration/V2__create_initial_tables.sql
+++ src/main/resources/db/migration/V2__create_initial_tables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE tb_events (
     id BIGSERIAL PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
-    date TIMESTAMP NOT NULL DEFAULT now(),
+    date TIMESTAMP NOT NULL,
     location VARCHAR(150) NOT NULL,
     capacity INT NOT NULL,
     available_tickets INT NOT NULL,
@@ -17,11 +17,11 @@ CREATE TABLE tb_customers (
 
 CREATE TABLE tb_tickets (
     id BIGSERIAL PRIMARY KEY,
-    code VARCHAR(36) UNIQUE,
+    code VARCHAR(36) NOT NULL UNIQUE,
     status VARCHAR(20) NOT NULL,
     purchase_date TIMESTAMP NOT NULL,
-    customer_id BIGINT REFERENCES tb_customers(id),
-    event_id BIGINT REFERENCES tb_events(id),
+    customer_id BIGINT,
+    event_id BIGINT,
 
     FOREIGN KEY (customer_id) REFERENCES tb_customers(id),
     FOREIGN KEY (event_id) REFERENCES tb_events(id)
