8b3145353415a69200be8e4f0a03cae58053d9c0
Pedro Henrique Molon Rodrigues <pedroh.molonr@gmail.com>
2026-09-16 10:34:13 -0300

    feat: add custom domain exceptions Refs #3

---
+++ src/main/java/com/pedromolon/eventticketsplatform/exception/BusinessException.java
@@ -0,0 +1,7 @@
+package com.pedromolon.eventticketsplatform.exception;
+
+public class BusinessException extends RuntimeException {
+    public BusinessException(String message) {
+        super(message);
+    }
+}
